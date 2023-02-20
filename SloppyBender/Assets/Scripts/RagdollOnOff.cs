445555be9aeb4a8591bfdcd2affed1f69389a582
PattenIan <106557564+PattenIan@users.noreply.github.com>
2023-02-20 19:11:35 +0100

    ragdoll working

---
+++ SloppyBender/Assets/Scripts/RagdollOnOff.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollOnOff : MonoBehaviour
+{
+    [SerializeField] BoxCollider mainCollider;
+    [SerializeField] GameObject rig;
+    [SerializeField] Animator animator;
+
+    Collider[] ragDollColliders;
+    Rigidbody[] limbsRigidBodies;
+    void Start()
+    {
+        GetRagdollBits();
+        RagDollOff();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void RagDollOn()
+    {
+        animator.enabled = false;
+        mainCollider.enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        foreach (Collider col in ragDollColliders)
+        {
+            col.enabled = true;
+        }
+        foreach (Rigidbody rigidbody in limbsRigidBodies)
+        {
+            rigidbody.isKinematic = false;
+        }
+    }
+
+    private void RagDollOff()
+    {
+        foreach(Collider col in ragDollColliders)
+        {
+            col.enabled = false;
+        }
+        foreach(Rigidbody rigidbody in limbsRigidBodies)
+        {
+            rigidbody.isKinematic = true;
+        }
+
+        animator.enabled = true;
+        mainCollider.enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    private void GetRagdollBits()
+    {
+        ragDollColliders = rig.GetComponentsInChildren<Collider>();
+        limbsRigidBodies = rig.GetComponentsInChildren<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "KnockDown")
+        {
+            RagDollOn();
+        }
+    }
+}
