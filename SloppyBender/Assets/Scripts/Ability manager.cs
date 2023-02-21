d4e695ae52ac833be3bd97665acc264fb547efe6
Noaskov <n.c.skov@gmail.com>
2023-02-21 22:09:05 +0100

    sad

--- SloppyBender/Assets/Scripts/Ability manager.cs
+++ SloppyBender/Assets/Scripts/Ability manager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Abilitymanager : MonoBehaviour
+public class AbilityManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] AbilityState Ability;
+    private Rigidbody rb;
+    [SerializeField] float JumpForce;
+    [SerializeField] Transform orientation;
+    private float JumpMissDirection;
+    private float JumpMissDirectionAmount;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public enum AbilityState
     {
         
+        Speed,
+        IceFingers,
+        FireFingers,
+        WaterFingers,
+        Strength
+
     }
 
-    // Update is called once per frame
+    void StateHandler()
+    {
+        Ability = AbilityState.Strength;
+    }
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) && Ability == AbilityState.Strength) 
+        {
+            Jump();
+            
+        }
+    }
+
+    void Jump()
+    {
+        JumpMissDirection = Random.Range(1, 3);
+        JumpMissDirectionAmount= Random.Range(-111, 112);
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+        if (JumpMissDirection == 2) 
+        {
+            rb.AddForce(orientation.forward * JumpMissDirection* 100, ForceMode.VelocityChange);
+        }
+        else
+        {
+            rb.AddForce(orientation.right * JumpMissDirection* 100, ForceMode.VelocityChange);
+        }
     }
 }
